b388f5aca3a0b0a14432fd984efac10206daea0d
Anas Mahmoud <nas@gra-login2.graham.sharcnet>
2021-08-24 10:34:06 -0400

    change default gpu from t4 to p100

--- tools/scripts/compute_canada_train_eval.sh
+++ tools/scripts/compute_canada_train_eval.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=OpenPCDet-train
 #SBATCH --account=rrg-swasland
 #SBATCH --cpus-per-task=8              # CPU cores/threads
-#SBATCH --gres=gpu:t4:2                # Number of GPUs (per node)
+#SBATCH --gres=gpu:p100:2                # Number of GPUs (per node)
 #SBATCH --mem=64000M                   # memory per node
 #SBATCH --output=./output/log/%x-%j.out   # STDOUT
 #SBATCH --mail-type=ALL
