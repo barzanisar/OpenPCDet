a6fcac8b36ff196b611ce8d275216b4211683830
nazmahmoud <naz.mahmoud@icloud.com>
2021-09-02 10:21:06 -0400

    clean up

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,5 +1,5 @@
 from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
-from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x, VoxelBackBone8xFuse, VoxelBackBone8xFuseConcatWeightedVoxelsSMALL, VoxelBackBone8xFuseConcatWeightedVoxelsBIG, VoxelBackBone8xFuseConcatForegroundWeight
+from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x, VoxelBackBone8xFuse
 from .spconv_unet import UNetV2
 
 __all__ = {
@@ -8,8 +8,5 @@ __all__ = {
     'UNetV2': UNetV2,
     'PointNet2Backbone': PointNet2Backbone,
     'PointNet2MSG': PointNet2MSG,
-    'VoxelResBackBone8x': VoxelResBackBone8x,
-    'VoxelBackBone8xFuseConcatWeightedVoxelsSMALL': VoxelBackBone8xFuseConcatWeightedVoxelsSMALL,
-    'VoxelBackBone8xFuseConcatWeightedVoxelsBIG': VoxelBackBone8xFuseConcatWeightedVoxelsBIG,
-    "VoxelBackBone8xFuseConcatForegroundWeight": VoxelBackBone8xFuseConcatForegroundWeight
+    'VoxelResBackBone8x': VoxelResBackBone8x
 }
