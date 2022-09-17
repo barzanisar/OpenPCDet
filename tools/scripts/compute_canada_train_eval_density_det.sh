5d3af73101dc0aa6d9a8bc514bd66ddf4e8e5493
Barza Nisar <barzanisar93@gmail.com>
2022-09-16 22:54:22 -0400

    finetune on 5 and 10% data

--- tools/scripts/compute_canada_train_eval_density_det.sh
+++ tools/scripts/compute_canada_train_eval_density_det.sh
@@ -28,7 +28,7 @@ SYNC_BN=true
 FIX_RANDOM_SEED=false
 #Save last 20 ckpts with interval 1
 CKPT_SAVE_INTERVAL=1
-MAX_CKPT_SAVE_NUM=20
+MAX_CKPT_SAVE_NUM=80
 SAVE_CKPT_AFTER_EPOCH=0
 # Epoch is evaluated if epoch > start epoch = 160 - NUM_EPOCHS_TO_EVAL
 NUM_EPOCHS_TO_EVAL=0
