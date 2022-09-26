da16dc84f3419fc7516833c3edd0563b3a65512f
Barza Nisar <barzanisar93@gmail.com>
2022-09-26 11:01:29 -0400

    make 1,5,10,20 % of train clear as well

--- data/dense/combine_train_infos.py
+++ data/dense/combine_train_infos.py
@@ -4,24 +4,25 @@ import numpy as np
 np.random.seed(100)
 root_path = "/home/barza/OpenPCDet/data/dense"
 split_base_mode = ['train', '60'] #['train', '60'] #['test', '25'] #['val', '15']
-percent_list = [1] #[5, 10, 20, 30, 40, 50, 100]  #[100]
-weather_splits = [f'{split_base_mode[0]}_light_fog_FOV3000_{split_base_mode[1]}.pkl', 
+percent_list = [1, 5, 10, 20, 30, 40, 50, 100]  #[100]
+weather_splits = [f'{split_base_mode[0]}_clear_FOV3000_{split_base_mode[1]}.pkl', 
+f'{split_base_mode[0]}_light_fog_FOV3000_{split_base_mode[1]}.pkl', 
 f'{split_base_mode[0]}_dense_fog_FOV3000_{split_base_mode[1]}.pkl', 
 f'{split_base_mode[0]}_snow_FOV3000_{split_base_mode[1]}.pkl']
 clear_infos =[]
 
-# Load all of clear weather split:
-info_path = root_path + f"/dense_infos_{split_base_mode[0]}_clear_FOV3000_{split_base_mode[1]}.pkl"
-with open(info_path, 'rb') as f:
-    infos = pickle.load(f)
-    clear_infos += infos
+# # Load all of clear weather split:
+# info_path = root_path + f"/dense_infos_{split_base_mode[0]}_clear_FOV3000_{split_base_mode[1]}.pkl"
+# with open(info_path, 'rb') as f:
+#     infos = pickle.load(f)
+#     clear_infos += infos
     
 
 for percent in percent_list:
     all_infos = []
-    all_infos += clear_infos
+    #all_infos += clear_infos
     print(f"All infos: {len(all_infos)}")
-    all_infos_path = root_path + f"/dense_infos_{split_base_mode[0]}_all_FOV3000_{split_base_mode[1]}"
+    all_infos_path = root_path + f"/dense_infos_{split_base_mode[0]}_all_splits_FOV3000_{split_base_mode[1]}"
     # Load a "percent" of other weather splits:
     for split in weather_splits:
         info_path = root_path + "/dense_infos_" + split
@@ -35,8 +36,9 @@ for percent in percent_list:
                 infos = (np.array(infos)[selected_idx]).tolist()
             print(f"Adding {split} infos: {len(infos)}")
             all_infos += infos
-            print(f"All infos: {len(all_infos)}")
+            #print(f"All infos: {len(all_infos)}")
 
+    print(f"For Percent {percent}, len all infos: {len(all_infos)}")
     if percent < 100:
         all_infos_path += '_' + str(percent)
     with open(all_infos_path + '.pkl', 'wb') as f:
