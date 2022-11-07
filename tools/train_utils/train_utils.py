683b344de51f049f405698b52140708dad3e844e
Barza Nisar <barzanisar93@gmail.com>
2022-11-07 15:13:18 -0500

    bug fix

--- tools/train_utils/train_utils.py
+++ tools/train_utils/train_utils.py
@@ -59,17 +59,12 @@ def train_one_epoch(cfg, cur_epoch, model, optimizer, train_loader, model_func, 
                 except:
                     fisher = ewc_params['fisher'][name[7:]]
                     optpar = ewc_params['optpar'][name[7:]]
-                print(f'Before Name: {name}, Rank: {rank}, Model {model.device}, Param {param.device}, Fisher {fisher.device}, {fisher.requires_grad}, OptPar {optpar.device}, {optpar.requires_grad}')
                 fisher = fisher.to(model.device)
                 optpar = optpar.to(model.device)
-                fisher.requires_grad = False
-                optpar.requires_grad = False
-                print(f'After Name: {name}, Rank: {rank}, Model {model.device}, Param {param.device}, Fisher {fisher.device}, {fisher.requires_grad}, OptPar {optpar.device}, {optpar.requires_grad}')
-                try:
-                    loss += (fisher * (optpar - param).pow(2)).sum() * cfg.EWC.LAMBDA
-                except:
-                    print(f'ERROR! Name: {name}, Rank: {rank}, Model {model.device}, Param {param.device}, Fisher {fisher.device}, OptPar {optpar.device}')
-                    loss += (fisher * (optpar - param).pow(2)).sum() * cfg.EWC.LAMBDA
+                assert(fisher.requires_grad == False)
+                assert(optpar.requires_grad == False)
+                
+                loss += (fisher * (optpar - param).pow(2)).sum() * cfg.EWC.LAMBDA
 
 
         cur_forward_time = time.time() - data_timer
