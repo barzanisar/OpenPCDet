1e400051c234fd635dde63888ea8a0688db6c925
Barza Nisar <barzanisar93@gmail.com>
2022-11-09 00:08:44 -0500

    train 5 other splits

--- train_kitti.sh
+++ train_kitti.sh
@@ -2,15 +2,22 @@
 # sbatch --time=03:00:00 --gres=gpu:t4:4 --array=1-5%1 --job-name=kitti-pointrcnn tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn.yaml --tcp_port 18461 --ckpt_save_interval 1 --fix_random_seed
 
 # Test Pointrcnn Kitti 100%
-sbatch --time=02:00:00 --array=1-4%1 --job-name=kitti-pointrcnn-val  tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn.yaml --tcp_port 18795 --fix_random_seed --test_only --eval_tag kitti_infos_val
+#sbatch --time=02:00:00 --array=1-4%1 --job-name=kitti-pointrcnn-val  tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn.yaml --tcp_port 18795 --fix_random_seed --test_only --eval_tag kitti_infos_val
 
 # Train Pointrcnn Kitti 5%
-sbatch --time=02:00:00 --gres=gpu:t4:4 --array=1-2%1 --job-name=kitti-pointrcnn_train_5_200epochs tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_200epochs.yaml --tcp_port 18465 --ckpt_save_interval 2 --fix_random_seed
-sbatch --time=02:00:00 --gres=gpu:t4:4 --array=1-5%1 --job-name=kitti-pointrcnn_train_5_500epochs tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_500epochs.yaml --tcp_port 18475 --ckpt_save_interval 6 --fix_random_seed
-sbatch --time=02:00:00 --gres=gpu:t4:4 --array=1-10%1 --job-name=kitti-pointrcnn_train_5_1000epochs tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_1000epochs.yaml --tcp_port 18485 --ckpt_save_interval 12 --fix_random_seed
-sbatch --time=02:00:00 --gres=gpu:t4:4 --array=1-15%1 --job-name=kitti-pointrcnn_train_5_1600epochs tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_1600epochs.yaml --tcp_port 18495 --ckpt_save_interval 20 --fix_random_seed
+sbatch --time=02:00:00 --gres=gpu:t4:4 --array=1-2%1 --job-name=kitti-pointrcnn_train_5_1_200epochs tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_1_200epochs.yaml --tcp_port 18465 --ckpt_save_interval 2 --fix_random_seed
+sbatch --time=02:00:00 --gres=gpu:t4:4 --array=1-2%1 --job-name=kitti-pointrcnn_train_5_2_200epochs tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_2_200epochs.yaml --tcp_port 18475 --ckpt_save_interval 2 --fix_random_seed
+
+
+# sbatch --time=02:00:00 --gres=gpu:t4:4 --array=1-2%1 --job-name=kitti-pointrcnn_train_5_200epochs tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_200epochs.yaml --tcp_port 18465 --ckpt_save_interval 2 --fix_random_seed
+# sbatch --time=02:00:00 --gres=gpu:t4:4 --array=1-5%1 --job-name=kitti-pointrcnn_train_5_500epochs tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_500epochs.yaml --tcp_port 18475 --ckpt_save_interval 6 --fix_random_seed
+# sbatch --time=02:00:00 --gres=gpu:t4:4 --array=1-10%1 --job-name=kitti-pointrcnn_train_5_1000epochs tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_1000epochs.yaml --tcp_port 18485 --ckpt_save_interval 12 --fix_random_seed
+# sbatch --time=02:00:00 --gres=gpu:t4:4 --array=1-20%1 --job-name=kitti-pointrcnn_train_5_1600epochs tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_1600epochs.yaml --tcp_port 18495 --ckpt_save_interval 20 --fix_random_seed
 
 # # TODO: Test Pointrcnn Kitti 5%
+# sbatch --time=02:00:00 --array=1-4%1 --job-name=kitti-pointrcnn_train_5_1_200epochs-val  tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_1_200epochs.yaml --tcp_port 18781 --fix_random_seed --test_only --eval_tag kitti_infos_val
+# sbatch --time=02:00:00 --array=1-4%1 --job-name=kitti-pointrcnn_train_5_2_200epochs-val  tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_2_200epochs.yaml --tcp_port 18791 --fix_random_seed --test_only --eval_tag kitti_infos_val
+
 # sbatch --time=02:00:00 --array=1-4%1 --job-name=kitti-pointrcnn_train_5_200epochs-val  tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_200epochs.yaml --tcp_port 18791 --fix_random_seed --test_only --eval_tag kitti_infos_val
 # sbatch --time=02:00:00 --array=1-4%1 --job-name=kitti-pointrcnn_train_5_500epochs-val  tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_500epochs.yaml --tcp_port 18792 --fix_random_seed --test_only --eval_tag kitti_infos_val
 # sbatch --time=02:00:00 --array=1-4%1 --job-name=kitti-pointrcnn_train_5_1000epochs-val  tools/scripts/compute_canada_train_eval_kitti.sh --cfg_file tools/cfgs/kitti_models/pointrcnn_train_5_1000epochs.yaml --tcp_port 18793 --fix_random_seed --test_only --eval_tag kitti_infos_val
