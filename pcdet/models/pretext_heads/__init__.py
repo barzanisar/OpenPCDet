62e132cc5611d197d07e2eb7bbd3fcf240bc130c
Barza Nisar <barzanisar93@gmail.com>
2023-07-09 17:12:12 -0400

    remove bbox head

--- pcdet/models/pretext_heads/__init__.py
+++ pcdet/models/pretext_heads/__init__.py
@@ -1,10 +1,8 @@
 from .seg_head import SegHead
-from .bbox_head import BboxHead
 from .depth_head import DepthHead
 
 
 __all__ = {
     'SegHead': SegHead,
-    'BboxHead': BboxHead,
     'DepthHead': DepthHead,
 }
