f78e84e71831e8b6cfe25cd25c3a5b51f1b37ec4
Barza Nisar <barzanisar93@gmail.com>
2023-10-27 02:16:34 -0400

    create database without parallel

--- train_waymo_project.sh
+++ train_waymo_project.sh
@@ -1,3 +1,3 @@
 #sbatch --time=3:00:00 --array=1-1%1 --job-name=pvrcnn-waymo tools/scripts/compute_canada_train_eval_waymo_project.sh --cfg_file tools/cfgs/waymo_models/pv_rcnn.yaml --tcp_port 18810
 
-sbatch --time=5:00:00 --array=1-1%1 --job-name=create_database-waymo tools/scripts/create_waymo_infos.sh
+sbatch --time=20:00:00 --array=1-1%1 --job-name=create_database-waymo tools/scripts/create_waymo_infos.sh
