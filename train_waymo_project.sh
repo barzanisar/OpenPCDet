baa9bb90958ffe8332fa161e8c5a0be09ecf0312
Barza Nisar <barzanisar93@gmail.com>
2023-10-28 13:53:08 -0400

    sbatch cmds to scratch train on 1percent

--- train_waymo_project.sh
+++ train_waymo_project.sh
@@ -1,4 +1,7 @@
 #sbatch --time=03:00:00 --array=1-1%1 --job-name=create_database-waymo tools/scripts/create_waymo_infos.sh
 
-sbatch --time=00:30:00 --nodes=1 --ntasks=1 --array=1-1%1 --job-name=pointrcnn-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_1percent.yaml --tcp_port 18810
-sbatch --time=00:30:00 --nodes=1 --ntasks=1 --array=1-1%1 --job-name=centerpoint-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/centerpoint_1percent.yaml --tcp_port 18890
+sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-5%1 --job-name=pointrcnn-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_1percent.yaml --tcp_port 18810
+sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-5%1 --job-name=centerpoint-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/centerpoint_1percent.yaml --tcp_port 18890
+
+sbatch --time=00:15:00 --nodes=1 --ntasks=1 --array=1-1%1 --job-name=pointrcnn-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_1percent.yaml --tcp_port 18810
+sbatch --time=00:15:00 --nodes=1 --ntasks=1 --array=1-1%1 --job-name=centerpoint-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/centerpoint_1percent.yaml --tcp_port 18890
