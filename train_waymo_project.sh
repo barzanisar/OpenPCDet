9ebbeda67eda07db8be8460f20db36e9ea240f92
Barza Nisar <barzanisar93@gmail.com>
2023-10-29 21:19:22 -0400

    only finetune on narval

--- train_waymo_project.sh
+++ train_waymo_project.sh
@@ -5,9 +5,9 @@
 # sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=pointrcnn-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_1percent.yaml --tcp_port 18810
 # sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=pointrcnn-clus-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_clus_1percent.yaml --tcp_port 18830
 
-# Test
-sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=test-pointrcnn-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_1percent.yaml --tcp_port 18810 --test_only 
-sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=test-pointrcnn-clus-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_clus_1percent.yaml --tcp_port 18830 --test_only
+# Test (not needed, check results from graham)
+# sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=test-pointrcnn-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_1percent.yaml --tcp_port 18810 --test_only 
+# sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=test-pointrcnn-clus-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_clus_1percent.yaml --tcp_port 18830 --test_only
 
 #### In progress #########
 #sbatch --time=03:00:00 --nodes=2 --ntasks=2 --array=1-2%1 --job-name=centerpoint-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/centerpoint_1percent.yaml --tcp_port 18820 --workers 2
@@ -17,7 +17,7 @@ sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=test-pointr
 # sbatch --time=04:00:00 --gres=gpu:t4:4 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=pointrcnn-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_1percent.yaml --tcp_port 18810
 # sbatch --time=04:00:00 --gres=gpu:t4:4 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=pointrcnn-clus-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_clus_1percent.yaml --tcp_port 18830
 
-# Test
+# Test (In progress)
 #sbatch --time=04:00:00 --gres=gpu:t4:4 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=test-pointrcnn-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_1percent.yaml --tcp_port 18810 --test_only 
 #sbatch --time=04:00:00 --gres=gpu:t4:4 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=test-pointrcnn-clus-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_clus_1percent.yaml --tcp_port 18830 --test_only
 
@@ -26,7 +26,7 @@ sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=test-pointr
 # sbatch --time=03:00:00 --gres=gpu:t4:4 --nodes=2 --ntasks=2 --array=1-3%1 --job-name=centerpoint-clus-waymo tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/centerpoint_clus_1percent.yaml --tcp_port 18840 --workers 2 --batch_size 4
 
 
-######################## Finetune on narval ###########################
+######################## Finetune on narval (In progress) ###########################
 sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=finetune-pointrcnn tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_clus_finetune_100ep_lr0p003.yaml --tcp_port 18910 --pretrained_model /OpenPCDet/checkpoints/pointrcnn_pretrain_ep99.pth.tar
 sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=finetune-pointrcnn tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_clus_finetune_200ep_lr0p003.yaml --tcp_port 18920 --pretrained_model /OpenPCDet/checkpoints/pointrcnn_pretrain_ep99.pth.tar
 sbatch --time=04:00:00 --nodes=2 --ntasks=2 --array=1-1%1 --job-name=finetune-pointrcnn tools/scripts/submit_ddp_compute_canada_waymo_multinode.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_clus_finetune_200ep_lr0p01.yaml --tcp_port 18930 --pretrained_model /OpenPCDet/checkpoints/pointrcnn_pretrain_ep99.pth.tar
