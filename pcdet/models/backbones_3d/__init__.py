53228caa698d439abf80750c3854921536469813
Barza Nisar <barzanisar93@gmail.com>
2023-12-03 14:19:15 -0500

    remove shape stuff cz image does not have ME yet

--- pcdet/models/backbones_3d/__init__.py
+++ pcdet/models/backbones_3d/__init__.py
@@ -1,7 +1,7 @@
 from .pointnet2_backbone import PointNet2Backbone, PointNet2MSG
 from .spconv_backbone import VoxelBackBone8x, VoxelResBackBone8x
 from .spconv_unet import UNetV2
-from .minkunet import MinkUNet
+# from .minkunet import MinkUNet
 
 __all__ = {
     'VoxelBackBone8x': VoxelBackBone8x,
@@ -9,6 +9,6 @@ __all__ = {
     'PointNet2Backbone': PointNet2Backbone,
     'PointNet2MSG': PointNet2MSG,
     'VoxelResBackBone8x': VoxelResBackBone8x,
-    'MinkUNet': MinkUNet
+    # 'MinkUNet': MinkUNet
 
 }
