c1a7fa636058ba908403de58ba12de86ece264ca
Barza Nisar <barzanisar93@gmail.com>
2024-02-08 21:58:45 -0500

    bug in waymo sampled (scene) and add 30 epochs

--- train_ssl_mink.sh
+++ train_ssl_mink.sh
@@ -10,6 +10,7 @@ sbatch --time=01:00:00 --array=1-1%1 --job-name=create_nuscenes_infos_1sweep_100
 #################### 80 epochs #########################################
 ## scratch
 sbatch --time=10:00:00 --array=1-2%1 --job-name=det-scratch-80ep-scene100 tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_80ep_scene_sampled_100.yaml --tcp_port 18910 --extra_tag scratch 
+sbatch --time=10:00:00 --array=1-2%1 --job-name=det-scratch-80ep-scene100_try2 tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_80ep_scene_sampled_100.yaml --tcp_port 19910 --extra_tag scratch_try2 
 
 ## finetune seg 
 sbatch --time=10:00:00 --array=1-2%1 --job-name=det-seg-80ep-scene100 tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_80ep_scene_sampled_100.yaml --tcp_port 18911 --extra_tag segcontrast --pretrained_model /OpenPCDet/checkpoints/minkunet_pretrain_ep99.pth.tar
@@ -18,6 +19,8 @@ sbatch --time=10:00:00 --array=1-2%1 --job-name=det-dc-80ep-scene100 tools/scrip
 sbatch --time=10:00:00 --array=1-2%1 --job-name=det-dc_det0p5-80ep-scene100 tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_80ep_scene_sampled_100.yaml --tcp_port 18914 --extra_tag depthcontrast_plus_dethead --pretrained_model /OpenPCDet/checkpoints/minkunet_pretrain_ep99.pth.tar
 
 sbatch --time=10:00:00 --array=1-1%1 --job-name=det-scratch-80ep tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_80ep_sampled_100.yaml --tcp_port 18911 --extra_tag scratch 
+sbatch --time=10:00:00 --array=1-1%1 --job-name=det-scratch-80ep_try2 tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_80ep_sampled_100.yaml --tcp_port 19911 --extra_tag scratch_try2 
+
 # sbatch --time=10:00:00 --array=1-4%1 --job-name=det-seg-80ep tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_80ep_sampled_100.yaml --tcp_port 18911 --extra_tag segcontrast --pretrained_model /OpenPCDet/checkpoints/minkunet_pretrain_ep99.pth.tar
 # sbatch --time=10:00:00 --array=1-4%1 --job-name=det-seg_plus_det0p5-80ep tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_80ep_sampled_100.yaml --tcp_port 18912 --extra_tag segcontrast_plus_dethead --pretrained_model /OpenPCDet/checkpoints/minkunet_pretrain_ep99.pth.tar
 # sbatch --time=10:00:00 --array=1-4%1 --job-name=det-dc-80ep tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_80ep_sampled_100.yaml --tcp_port 18913 --extra_tag depthcontrast --pretrained_model /OpenPCDet/checkpoints/minkunet_pretrain_ep99.pth.tar
@@ -33,6 +36,7 @@ sbatch --time=10:00:00 --array=1-1%1 --job-name=det-scratch-80ep tools/scripts/s
 #################### 200 epochs #########################################
 ## scratch
 sbatch --time=10:00:00 --array=1-3%1 --job-name=det-scratch-200ep-scene100 tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_200ep_scene_sampled_100.yaml --tcp_port 18710 --extra_tag scratch
+sbatch --time=10:00:00 --array=1-3%1 --job-name=det-scratch-200ep-scene100_try2 tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_200ep_scene_sampled_100.yaml --tcp_port 17710 --extra_tag scratch_try2
 
 ## finetune seg 
 sbatch --time=10:00:00 --array=1-4%1 --job-name=det-seg-200ep-scene100 tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_200ep_scene_sampled_100.yaml --tcp_port 18711 --extra_tag segcontrast --pretrained_model /OpenPCDet/checkpoints/minkunet_pretrain_ep99.pth.tar
@@ -42,9 +46,18 @@ sbatch --time=10:00:00 --array=1-4%1 --job-name=det-dc_det0p5-200ep-scene100 too
 
 
 sbatch --time=10:00:00 --array=1-4%1 --job-name=det-scratch-200ep tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_200ep_sampled_100.yaml --tcp_port 18810 --extra_tag scratch 
+sbatch --time=10:00:00 --array=1-4%1 --job-name=det-scratch-200ep_try2 tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_200ep_sampled_100.yaml --tcp_port 18811 --extra_tag scratch_try2
+
 # sbatch --time=10:00:00 --array=1-4%1 --job-name=det-seg-200ep tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_200ep_sampled_100.yaml --tcp_port 18811 --extra_tag segcontrast --pretrained_model /OpenPCDet/checkpoints/minkunet_pretrain_ep99.pth.tar
 # sbatch --time=10:00:00 --array=1-4%1 --job-name=det-seg_plus_det0p5-200ep tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_200ep_sampled_100.yaml --tcp_port 18812 --extra_tag segcontrast_plus_dethead --pretrained_model /OpenPCDet/checkpoints/minkunet_pretrain_ep99.pth.tar
 # sbatch --time=10:00:00 --array=1-4%1 --job-name=det-dc-200ep tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_200ep_sampled_100.yaml --tcp_port 18813 --extra_tag depthcontrast --pretrained_model /OpenPCDet/checkpoints/minkunet_pretrain_ep99.pth.tar
 # sbatch --time=10:00:00 --array=1-4%1 --job-name=det-dc_det0p5-200ep tools/scripts/submit_ddp_$CLUSTER_NAME.sh --cfg_file tools/cfgs/waymo_models/pointrcnn_minkunet_200ep_sampled_100.yaml --tcp_port 18814 --extra_tag depthcontrast_plus_dethead --pretrained_model /OpenPCDet/checkpoints/minkunet_pretrain_ep99.pth.tar
 
-########################## nuscenes #########################################
+
+##### 30 epochs waymo sampled 100 i.e. 1% (for pretrained models)######
+########################## waymo sampled 20 i.e. 5%  #########################################
+### 30, 80, 200 epochs
+########################## nuscenes sampled 100 i.e. 1%  #########################################
+### 30, 80, 200 epochs
+########################## nuscenes sampled 20 i.e. 5%  #########################################
+### 30, 80, 200 epochs
